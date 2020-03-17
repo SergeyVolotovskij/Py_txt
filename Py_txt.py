a6634ded02c7615ba9bd419fee41477932bcac30
SergeyVolotovskij <61363925+SergeyVolotovskij@users.noreply.github.com>
2020-03-17 07:39:15 +0300

    Upgrate

--- Py_txt.py
+++ Py_txt.py
@@ -1,50 +1,56 @@
 from datetime import *
-from colorama import init #для стиля ветового
-from colorama import Fore, Back, Style #для стиля ветового
-init()#для стиля цветового
+from colorama import init  # для стиля цветового
+from colorama import Fore, Back, Style  # для стиля цветового
+
+init()  # для стиля цветового
 
 """ Cоздает новый файл именуя его как "task" + _тек.дата.txt """
-datenow = datetime.today().strftime(" %d.%m.%Y") #дата-месяц-год
-filename = "Task" + "_" + str(datenow) + ".txt"
+datenow=datetime.today().strftime(" %d.%m.%Y")  # дата-месяц-год
+filename="Task" + "_" + str(datenow) + ".txt"
 
-#ДОБАВЛЯЕМ ПРОВЕРКУ - СУЩЕСТВУЕТ УЖЕ ТАКОЙ ФАЙЛ ИЛИ НЕТ?
+# ДОБАВЛЯЕМ ПРОВЕРКУ - СУЩЕСТВУЕТ УЖЕ ТАКОЙ ФАЙЛ ИЛИ НЕТ?
 try:
-    file = open(filename, "r")
-except FileNotFoundError: #ЕСЛИ ФАЙЛА НЕТУ - ТОГДА...
+    file=open(filename, "r")
+except FileNotFoundError:  # ЕСЛИ ФАЙЛА НЕТУ - ТОГДА...
     print("Файл отсутствует - формирую новый!")
-    file = open(filename, "w") #СОЗДАЕМ ФАЙЛ
+    file=open(filename, "w")  # СОЗДАЕМ ФАЙЛ
 
-    #ОТКРЫВАЕМ ФАЙЛ ДЛЯ ЗАПОЛНЕНИЯ ШАПКИ
-    file = open(filename,"a")
+    # ОТКРЫВАЕМ ФАЙЛ ДЛЯ ЗАПОЛНЕНИЯ ШАПКИ
+    file=open(filename, "a")
     file.write(('{0:*^20}').format(datenow + "г. "))
     file.write("\n" + "========================================" + "\n")
     file.close()
+
 
 def write_file():
     """ Функция производит запись информации, введенную пользователем в файл методом
     добавления """
     print(Fore.YELLOW)
-    info = input("КАКУЮ ИНФОРМАЦИЮ ВВЕСТИ? ")
-    info1 = input("ВАЖНО? ")
-    file = open(filename, "a")
+    info=input("КАКУЮ ИНФОРМАЦИЮ ВВЕСТИ? ")
+    info1=input("ВАЖНО? ")
+    file=open(filename, "a")
 
     if info1 == "да":
-        file.write("!" + "*" + ". " + info + ".\n")
+        file.write("!" + "*" + ". " + info.upper() + ".\n")
+        print(Fore.GREEN)
+        print("ВАЖНАЯ ИНФОРМАЦИЯ ВНЕСЕНА!")
 
     elif info1 == "да1":
-        file.write("!" + "*" + ". " + info + ".\n")
-        file = open(filename, "r")
+        file.write("!" + "*" + ". " + info.upper() + ".\n")
+        file=open(filename, "r")
+        print(Fore.GREEN)
+        print("ВАЖНАЯ ИНФОРМАЦИЯ ВНЕСЕНА!")
 
         print(Fore.YELLOW)
-        print(file.read()) #Выводим инфо с файла в консоль!!!
+        print(file.read())  # Выводим инфо с файла в консоль!!!
 
     else:
-        file.write("*" + ". " + info + ".\n")
+        file.write("*" + ". " + info.upper() + ".\n")
 
-    print(Fore.GREEN)
-    print("ИНФОРМАЦИЯ ВНЕСЕНА!")
+        print(Fore.GREEN)
+        print("ИНФОРМАЦИЯ ВНЕСЕНА!")
     file.close()
 
-while 1 == 1:
-	write_file()
 
+while 1 == 1:
+    write_file()
